4e07d6093e20abd014ee68011f86177ed3cdead9
Ke Wang <wangkekekexili@gmail.com>
2018-12-10 21:28:26 -0500

    Improve edit distance.

--- match/proximate/edit_distance.go
+++ match/proximate/edit_distance.go
@@ -9,29 +9,28 @@ func EditDistance(left, right []byte) int {
 		return 0
 	}
 
-	var distance [][]int
-	for i := 0; i <= len(left); i++ {
-		distance = append(distance, make([]int, len(right)+1))
-		distance[i][0] = i
-	}
+	previous := make([]int, len(right)+1)
 	for j := 0; j <= len(right); j++ {
-		distance[0][j] = j
+		previous[j] = j
 	}
 	for i := 1; i <= len(left); i++ {
+		current := make([]int, len(right)+1)
+		current[0] = i
 		for j := 1; j <= len(right); j++ {
 			if left[i-1] == right[j-1] {
-				distance[i][j] = distance[i-1][j-1]
+				current[j] = previous[j-1]
 			} else {
-				distance[i][j] = distance[i-1][j-1] + 1
+				current[j] = previous[j-1] + 1
 			}
-			if distance[i][j-1]+1 < distance[i][j] {
-				distance[i][j] = distance[i][j-1] + 1
+			if previous[j]+1 < current[j] {
+				current[j] = previous[j] + 1
 			}
-			if distance[i-1][j]+1 < distance[i][j] {
-				distance[i][j] = distance[i-1][j] + 1
+			if current[j-1]+1 < current[j] {
+				current[j] = current[j-1] + 1
 			}
 		}
+		previous = current
 	}
 
-	return distance[len(left)][len(right)]
+	return previous[len(right)]
 }
--- match/proximate/edit_distance_test.go
+++ match/proximate/edit_distance_test.go
@@ -13,11 +13,12 @@ func TestEditDistance(t *testing.T) {
 		{[]byte{'A'}, []byte{'C', 'A'}, 1},
 		{[]byte{'A', 'G'}, []byte{'C', 'T'}, 2},
 		{[]byte{'A', 'C', 'G', 'T'}, []byte{'A', 'T'}, 2},
+		{[]byte{'A', 'A', 'C', 'G', 'A'}, []byte{'A', 'C', 'G', 'T', 'A'}, 2},
 	}
 	for _, test := range tests {
 		gotDistance := EditDistance(test.left, test.right)
 		if gotDistance != test.expDistance {
-			t.Fatalf("got distance %v; want %v", gotDistance, test.expDistance)
+			t.Fatalf("%q %q got distance %v; want %v", test.left, test.right, gotDistance, test.expDistance)
 		}
 	}
 }
